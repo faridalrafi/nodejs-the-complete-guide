c22a738edd43e4df36eb9206c040b66be07557fb
Yago Souza Santos <6200092+YSOUZAS@users.noreply.github.com>
2018-11-07 23:37:47 +0000

    feat(routes): adiciona rotas iniciais
    adicionando rota admin e shop

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page Not Found</h1>');
+});
 
 app.listen(3000);
