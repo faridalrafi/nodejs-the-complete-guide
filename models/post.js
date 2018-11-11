390e127f6a9cf091cf369f0a964a9a270b0b9e55
Yago Souza Santos <6200092+YSOUZAS@users.noreply.github.com>
2018-11-11 09:24:32 +0000

    feat(api) adicionando upload de imagens

--- models/post.js
+++ models/post.js
@@ -3,10 +3,22 @@ const Schema = mongoose.Schema;
 
 const postSchema = new Schema(
   {
-    title: { type: String, required: true },
-    imageUrl: { type: String, required: true },
-    content: { type: String, required: true },
-    creator: { type: Object, required: String }
+    title: {
+      type: String,
+      required: true
+    },
+    imageUrl: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    creator: {
+      type: Object,
+      required: String
+    }
   },
   { timestamps: true }
 );
